d2e940e40d405e9e1a9346015f79c715052b0538
Alexandra Weaver <aldenweaver@users.noreply.github.com>
2023-05-06 17:13:10 -0400

    Added practice project rails-url-helpers-lab-seattle-web-120919

---
+++ rails-url-helpers-lab-seattle-web-120919/app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: :show
+  
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Post.find(params[:id])
+
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+end
